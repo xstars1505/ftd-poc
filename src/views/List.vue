a7714f7d07994ce8617b3b8928c4c8fe47897184
Minh Le Tran Ba <minhltb@tech.est-rouge.com>
2020-07-22 11:38:30 +0700

    Add offset

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,17 +1,32 @@
 <template>
-  <DataTable :value="nodes" :paginator="true" :rows="5" :loading="loading">
-    <Column header="">
-      <template #body="node">
-        <router-link to="/details">#{{ node.data.id }}</router-link>
+  <div>
+    <div class="offset-container">
+      <label class="p-mr-1">表示件数:</label>
+      <Dropdown v-model="offset" :options="offsets" optionLabel="label" />
+    </div>
+    <DataTable
+      :value="nodes"
+      :rows="5"
+      :loading="loading"
+      :paginator="true"
+      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
+    >
+      <Column header="">
+        <template #body="node">
+          <router-link to="/details">#{{ node.data.id }}</router-link>
+        </template>
+      </Column>
+      <Column field="date" header="日時"></Column>
+      <Column field="customer" header="ユーザー"></Column>
+      <Column field="total" header="合計"></Column>
+      <Column field="quantity" header="数量"></Column>
+      <Column field="paymentStatus" header="請求状態"></Column>
+      <Column field="workMenuStatus" header="作業状態"></Column>
+      <template #paginatorLeft>
+        表示件数 3 件
       </template>
-    </Column>
-    <Column field="date" header="日時"></Column>
-    <Column field="customer" header="ユーザー"></Column>
-    <Column field="total" header="合計"></Column>
-    <Column field="quantity" header="数量"></Column>
-    <Column field="paymentStatus" header="請求状態"></Column>
-    <Column field="workMenuStatus" header="作業状態"></Column>
-  </DataTable>
+    </DataTable>
+  </div>
 </template>
 
 <script>
@@ -19,21 +34,29 @@ import router from "@/router";
 import NodeService from "@/services/NodeService";
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
+import Dropdown from "primevue/dropdown";
 
 export default {
   name: "List",
-  components: { DataTable, Column },
+  components: { DataTable, Column, Dropdown },
   data() {
     return {
       show: true,
       nodes: null,
       loading: true,
       selectedKeys3: null,
+      offset: null,
+      offsets: [
+        { label: "10 件", value: 10 },
+        { label: "20 件", value: 20 },
+        { label: "50 件", value: 50 }
+      ],
       selectedNodes: []
     };
   },
   nodeService: null,
   created() {
+    this.offset = this.offsets[0];
     this.nodeService = new NodeService();
   },
   mounted() {
--- src/views/WorkMenuList.vue
+++ src/views/WorkMenuList.vue
@@ -1,42 +1,65 @@
 <template>
-  <DataTable :value="nodes" :paginator="true" :rows="5" :loading="loading">
-    <Column header="">
-      <template #body="node"
-        ><a href="javascript:void(0)">#{{ node.data.id }}</a>
+  <div>
+    <div class="offset-container">
+      <label class="p-mr-1">表示件数:</label>
+      <Dropdown v-model="offset" :options="offsets" optionLabel="label" />
+    </div>
+    <DataTable
+      :value="nodes"
+      :paginator="true"
+      :rows="5"
+      :loading="loading"
+      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
+    >
+      <Column header="">
+        <template #body="node"
+          ><a href="javascript:void(0)">#{{ node.data.id }}</a>
+        </template>
+      </Column>
+      <Column field="date" header="受注日"></Column>
+      <Column field="quantity" header="数量"></Column>
+      <Column header="作業">
+        <template #body="node">
+          <ul>
+            <li v-for="workMenu in node.data.workMenus" :key="workMenu.id">
+              <label>{{ workMenu.name }}:</label>
+              <span> {{ workMenu.implementedQuantity }} </span>
+            </li>
+          </ul>
+        </template>
+      </Column>
+      <template #paginatorLeft>
+        表示件数 3 件
       </template>
-    </Column>
-    <Column field="date" header="受注日"></Column>
-    <Column field="quantity" header="数量"></Column>
-    <Column header="作業">
-      <template #body="node">
-        <ul>
-          <li v-for="workMenu in node.data.workMenus" :key="workMenu.id">
-            <label>{{ workMenu.name }}:</label>
-            <span> {{ workMenu.implementedQuantity }} </span>
-          </li>
-        </ul>
-      </template>
-    </Column>
-  </DataTable>
+    </DataTable>
+  </div>
 </template>
 
 <script>
 import NodeService from "@/services/NodeService";
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
+import Dropdown from "primevue/dropdown";
 
 export default {
   name: "WorkMenuList",
-  components: { DataTable, Column },
+  components: { DataTable, Column, Dropdown },
   data() {
     return {
       show: true,
       nodes: null,
+      offset: null,
+      offsets: [
+        { label: "10 件", value: 10 },
+        { label: "20 件", value: 20 },
+        { label: "50 件", value: 50 }
+      ],
       loading: true
     };
   },
   nodeService: null,
   created() {
+    this.offset = this.offsets[0];
     this.nodeService = new NodeService();
   },
   mounted() {
